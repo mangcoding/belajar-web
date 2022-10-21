ed10ba007fdf318ed3bc998bd5966320e58ecd61
mangcoding <mangcoding@gmail.com>
2022-10-21 09:42:58 +0700

    adding more changes

--- 2022/studi-cases/tailwind.config.js
+++ 2022/studi-cases/tailwind.config.js
@@ -2,15 +2,13 @@ tailwind.config = {
     theme: {
         extend: {
             colors: {
-                primary: '#99c6d8',
+                primary : '#99c6d8',
+                header : "#F6F9FC",
             },
             fontFamily: {
-                base: ['Nunito', 'sans-serif'],
-                nav: ['Raleway', 'sans-serif']
+                raleway: ['Raleway', 'sans-serif'],
+                nunito: ['Nunito Sans', 'sans-serif'],
             },
-            fontSize: {
-                'heading-1': '50px',
-            },
-        },
+        }
     }
 }
