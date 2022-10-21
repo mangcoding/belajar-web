f7c2393120155ef4acc063a0019a9e7f9b72f4fb
mangcoding <mangcoding@gmail.com>
2022-10-21 17:14:05 +0700

    update studi-cases

--- 2022/studi-cases/tailwind.config.js
+++ 2022/studi-cases/tailwind.config.js
@@ -2,13 +2,15 @@ tailwind.config = {
     theme: {
         extend: {
             colors: {
-                primary : '#99c6d8',
-                header : "#F6F9FC",
+                primary: '#0EC5D7',
+                secondary: '#ff3b07',
+                nav: '#969696',
+                header: '#F6F9FC',
             },
             fontFamily: {
                 raleway: ['Raleway', 'sans-serif'],
                 nunito: ['Nunito Sans', 'sans-serif'],
-            },
-        }
-    }
+            }
+        },
+    },
 }
